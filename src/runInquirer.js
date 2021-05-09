e9e53a0cf3cf1739384ca553af2fe30dafe9aacb
Andrew Ogilvie <primalorb@gmail.com>
2021-05-09 15:27:39 -0400

    add departments and add roles

--- src/runInquirer.js
+++ src/runInquirer.js
@@ -34,6 +34,12 @@ function initial() {
         case "View all employees":
             viewEmployees();
             break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Add a role":
+            addRole();
+            break;
         }
     })
 };
@@ -109,6 +115,120 @@ function viewEmployees() {
        .catch( err => {
            console.log( err );
        } );
+};
+
+    // add new department
+function addDepartment() {
+    return inquirer.prompt( [
+        {
+            type: 'text',
+            name: "department",
+            message: "What department would you like to add?",
+            validate: input => { // validate that an input has been entered
+                if( input ) {
+                    return true
+                } else {
+                    console.log( 'Enter a department name' )
+                    return false
+                }
+            }
+        }
+    ] )
+    .then( data => {
+            // collect response as string
+        const department = Object.values(data)[0]
+            // query string
+        db.promise().query(`INSERT INTO departments ( name ) VALUES ( ? )`, [ department ])
+           .then( ([rows,fields]) => {
+                    // display confirmation
+               console.log( `------------------
+${rows.affectedRows} department added
+------------------` )
+                    // return to menu
+               initial();
+           })
+           .catch( err => {
+               console.log( err );
+           } );
+    })
+
+};
+
+    // add new role
+function addRole() {
+    const roles = []
+        // collect department names from database for choices in inquirer prompt
+    db.promise().query(`SELECT name,id FROM departments;`)
+        .then( ([rows,fields]) => {
+            // create empty array
+        const choices = []   
+            // push choices into array
+        rows.forEach( entry => choices.push( Object.values(entry)[0] ))
+            // push results into array to search for ID after selection
+        rows.forEach( entry => roles.push( {id: Object.values(entry)[1], department: Object.values(entry)[0] } ) )
+            // return to menu
+         return inquirer.prompt( [
+                {
+                    type: 'text',
+                    name: "role",
+                    message: "What role would you like to add?",
+                    validate: input => { // validate that an input has been entered
+                        if( input ) {
+                            return true
+                        } else {
+                            console.log( 'Enter a role name' )
+                            return false
+                        }
+                    }
+                },
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: "What department does this belong to?",
+                    choices: choices,
+                    validate: input => {
+                        if( input.length === 1 ) { // check for length of 1 to validate single selection
+                            return true
+                        } else {
+                            console.log( 'Select a department!' )
+                            return false
+                        }
+                    }
+                },
+                {
+                    type: 'text',
+                    name: 'salary',
+                    message: "What salaray does this role offer?",
+                    validate: input => {
+                        if( !input === NaN || input > 0 ) {
+                            return true
+                        } else {
+                            console.log( 'Enter a salary value!' )
+                            return false
+                        }
+                    }
+                }
+            ] )
+        })
+        .then( data => {
+                // collect ID of department by
+            const departmentID = roles.filter( x => x.department === data.department)
+            db.promise().query(`INSERT INTO roles ( title, salary, department_id ) VALUES ( ?, ?, ? )`, [ data.role, data.salary, departmentID[0].id ])
+           .then( ([rows,fields]) => {
+                    // display confirmation
+               console.log( `------------------
+${rows.affectedRows} role added
+------------------` )
+                    // return to menu
+               initial();
+           })
+           .catch( err => {
+               console.log( err );
+           } );
+        })
+        .catch( err => {
+            console.log( err );
+        } );
 };
 
 module.exports = initial;
